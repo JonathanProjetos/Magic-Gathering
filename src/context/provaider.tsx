3e396d49905e288190a4fadc8d355cd575a8e26e
Jonathan Santos <jonathan01347@gmail.com>
2023-02-25 17:25:29 -0300

    refactor(provaider)add fetch getAllCards e getById

--- src/context/provaider.tsx
+++ src/context/provaider.tsx
@@ -1,20 +1,50 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useEffect, useState } from 'react'
 import Context from './context'
 import PropTypes from 'prop-types';
+import { requestCardByID, requestCards } from '../Api/request'
+import { Card } from '../Interface/Cards';
 
 type Props = {
   children: React.ReactNode;
 }
 
 function Provaider({ children }: Props) {
-  const [inputName, setInputName] = React.useState('')
+  const [inputName, setInputName] = useState('')
+  const [cards, setCards] = useState([])
 
+  const getAllCards = async () => {
+    try {
+      const data = await requestCards()
+      const dataFilterCards = data?.cards.filter((card: Card) => card.imageUrl !== undefined)
+      setCards(dataFilterCards)
+    
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getCardByID = async (id: string) => {
+    try {
+      const data = await requestCardByID(id)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=> {
+    getAllCards()
+  }, [])
+  
   const data = useMemo(() => ({
     inputName,
     setInputName,
+    getCardByID,
+    cards,
   }), [
     inputName, 
-    setInputName
+    setInputName,
+    cards,
   ])
 
   return (
