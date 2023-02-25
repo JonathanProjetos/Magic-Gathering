f89ea7c96da58053493e79d9c9b5127660ec8877
Jonathan Santos <jonathan01347@gmail.com>
2023-02-25 17:26:28 -0300

    feat(PropsContext)Incremente interface.

--- src/Interface/PropsContext.tsx
+++ src/Interface/PropsContext.tsx
@@ -1,4 +1,8 @@
+import { Card } from "../Interface/Cards";
+
 export interface PropsContext {
   inputName: string;
   setInputName: React.Dispatch<React.SetStateAction<string>>;
+  getCardByID: (id: string) => Promise<void>;
+  cards: Card[];
 }
