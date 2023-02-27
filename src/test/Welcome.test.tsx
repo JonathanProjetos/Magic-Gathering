af325854fae43c08c4ceaa38b9696783c5df514b
Jonathan Santos <jonathan01347@gmail.com>
2023-02-27 12:18:15 -0300

    test(Welcome)Adicionado mais tests para a page.

--- src/test/Welcome.test.tsx
+++ src/test/Welcome.test.tsx
@@ -1,13 +1,47 @@
-import {screen} from '@testing-library/react'
+import { screen, waitFor,waitForElementToBeRemoved } from '@testing-library/react'
 import renderWithRouter from './renderWithRouter'
-import { describe, it  } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { describe, it } from 'vitest'
 import App from '../App'
 
 describe('Page Welcome', ()=> {
-  it('Page Welcome', () => {
+
+  it('presença da messagem inicial', () => {
     renderWithRouter(<App />, ['/'])
-  
-    const title = screen.getByText(/Bem vindo ao catálogo de cards de Magic Gathering V1!/i)
-    expect(title).toBeInTheDocument()
+    
+    const TITLE = screen.getByText(/Bem vindo ao catálogo de cards de Magic Gathering V1!/i)
+    expect(TITLE).toBeInTheDocument()
   })
+  
+  it('É possivel digitar input name?', async () => {
+    renderWithRouter(<App />, ['/'])
+    
+    const INPUT = screen.getByRole('textbox')
+    expect(INPUT).toBeInTheDocument()
+
+    await userEvent.type(INPUT, 'testando')
+    expect(INPUT).toHaveValue('testando')
+  })
+  
+  it('Disabled quando input name tem a quantidade de caracters menor 5', () => {
+    renderWithRouter(<App />, ['/'])
+
+    const button = screen.getByRole('button', { name: /Entrar/i })
+    expect(button).toBeDisabled()
+  })
+
+  it('Botão de redirecionamento para a página de cards', async () => {
+   renderWithRouter(<App />, ['/'])
+    
+    const INPUT = screen.getByRole('textbox')
+    await userEvent.type(INPUT, 'teste')
+    
+    const button = screen.getByRole('button', { name: /Entrar/i })
+    userEvent.click(button)
+    expect(button).toBeInTheDocument()
+    expect(button).toBeEnabled()
+    
+  })
+
+
 })
