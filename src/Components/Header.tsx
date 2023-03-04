b26e1c0af8ac62be58706bb7030e84ee32d20c4f
Jonathan Santos <jonathan01347@gmail.com>
2023-03-03 21:50:26 -0300

    feat(Header)add componente header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import SingOut from './SingOut'
 
 function Header() {
+
   return (
-    <div>Header</div>
+    <div 
+      className='w-screen h-auto flex flex-row justify-between py-5'
+    >
+      <h1 className='ml-10 text-2xl'>
+        {
+          `Seja bem vindo! ${localStorage.getItem('name') || ''}.`
+        }
+      </h1>
+      <SingOut />
+    </div>
   )
 }
 
