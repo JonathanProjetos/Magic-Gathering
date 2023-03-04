2c496a58a8967de1781724b331ff9cad0b97d4a9
Jonathan Santos <jonathan01347@gmail.com>
2023-03-03 21:52:09 -0300

    feat(SingOut)add componente para sair

---
+++ src/Components/SingOut.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import icon from '../assets/icons/index'
+
+function SingOut() {
+
+  const navigate = useNavigate()
+
+  const clearLocalStorage = () => {
+    localStorage.setItem('name', '')
+    localStorage.setItem('idCard', '')
+    navigate('/', { replace: true })
+  }
+
+  return (
+    <div
+      className='mr-10'
+    >
+      <button
+        type='button'
+        onClick={() => clearLocalStorage()}
+      >
+        {
+          <img 
+            src={icon[0].logo} 
+            alt='sair'
+            className='w-10 h-10' 
+          />
+        }
+      </button>
+    </div>
+  )
+}
+
+export default SingOut
