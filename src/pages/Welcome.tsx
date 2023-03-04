ddba5cda98de6cbf4b3c09c1452ef58d9da97366
Jonathan Santos <jonathan01347@gmail.com>
2023-03-03 21:55:49 -0300

    style(Welcome)alterado css

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import Input from '../Components/Input'
 import ButtonRedirect from '../Components/ButtonRedirect'
+import images from '../assets'
 
 function Welcome() {
+
   return (
-    <section className="w-screen h-screen flex flex-col justify-center items-center">
-      Bem vindo ao catálogo de cards de Magic Gathering V1!
-      <Input/>
-      <ButtonRedirect/>
+    <section
+    className={'w-full h-screen flex flex-col justify-center items-center'}
+    style={{ 
+      backgroundImage: `url(${images[0].logo})`,
+      backgroundPositionX: 'center',
+      backgroundPositionY: '10vh',
+      backgroundRepeat: 'no-repeat',
+    }}
+    >
+      <div
+        className={'w-2vw h-4vh flex flex-col justify-center items-center'}
+        style={{
+          textAlign: 'center',
+          padding: '10px',
+        }}
+      >
+       Magic Gathering V1!
+        <Input/>
+        <ButtonRedirect/>
+      </div>
     </section>
   )
 }
