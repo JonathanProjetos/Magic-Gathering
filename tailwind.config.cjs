b6c441102d256d0f1f70b57885eefc68402a6427
Jonathan Santos <jonathan01347@gmail.com>
2023-03-03 21:47:09 -0300

    fix(tailwind)add configurações

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,12 +11,27 @@ module.exports = {
       },
       width: {
         '2xx': '30vw',
-        '2vw': '20vw'
+        '2vw': '20vw',
+        '2pxw': '200px'
       },
       height: {
         '2xx': '30vw',
-        '4vh': '40vh'
-      }
+        '4vh': '40vh',
+        '2pxh': '290px'
+      },
+      margin: {
+        '10px': '10px'
+      },
+      // screens: {
+      //   tablet: '640px',
+      //   // => @media (min-width: 640px) { ... }
+
+      //   laptop: '1024px',
+      //   // => @media (min-width: 1024px) { ... }
+
+      //   desktop: '1280px'
+      //   // => @media (min-width: 1280px) { ... }
+      // }
     }
   },
   plugins: []
